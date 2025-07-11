e6f82b26e5c6b0342f614fbaeb3c06bc2d58e64f
Rishabh <rxb.zb5w@gmail.com>
2025-07-11 14:17:02 +0530

    updated config/firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "demo-key",
-  authDomain: "holocart-demo.firebaseapp.com",
-  databaseURL: "https://holocart-demo-default-rtdb.firebaseio.com",
-  projectId: "holocart-demo",
-  storageBucket: "holocart-demo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyDlkq8FMoNPKFr3wl5Px-4PFAN2ne-Iza8",
+  authDomain: "holocart-9b9ba.firebaseapp.com",
+  databaseURL: "https://holocart-9b9ba-default-rtdb.firebaseio.com",
+  projectId: "holocart-9b9ba",
+  storageBucket: "holocart-9b9ba.firebasestorage.app",
+  messagingSenderId: "1010221461280",
+  appId: "1:1010221461280:web:e847effca1665838723145",
+  measurementId: "G-354KN94HRF"
 };
 
 // Initialize Firebase
